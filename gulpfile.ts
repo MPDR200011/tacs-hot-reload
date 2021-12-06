5092ae3105a4403b3a668e11a0da3a5783ee35be
Moisés Rocha <moisesrocha99@gmail.com>
2021-12-06 15:52:07 +0000

    Initial setup

---
+++ gulpfile.ts
@@ -0,0 +1,12 @@
+import { src, dest, watch } from 'gulp';
+
+function buildHtml() {
+    // place code for your default task here
+    src('src/*.html').pipe(dest('public'))
+}
+
+function defaultTask() {
+    watch('src/*.html', {ignoreInitial: false}, buildHtml)
+}
+
+export default defaultTask;
